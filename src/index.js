79a484e34ad31c2a47addb7c426069b12c6a383d
Sergey Zylevich <sergey496@yandex.ru>
2018-02-21 22:12:42 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
+	var sum=0;
+	var kol;
+	for (var i=1;i<12;i++)
+		{	
+			kol=Math.floor(number/Math.pow(5,i));
+			sum+=kol;
+		}
+return sum;
 }
